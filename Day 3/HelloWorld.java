e0d4ffc26727dbfdbf9f2cb760b1aa39754f94d9
barman <shubhambarman1117@gmail.com>
2023-07-20 23:35:24 +0530

    Day 4 Commit

--- Day 3/HelloWorld.java
+++ Day 3/HelloWorld.java
@@ -2,7 +2,14 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
 
